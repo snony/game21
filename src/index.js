06b14871ce876a52636d51d85a9d3145ab066d27
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-09-10 18:06:36 +0100

    I think that I have understood this one correctly?

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import './assets/kingOfSpade.png';
 
 
 class Card extends React.Component{
-
+    constructor(props){
+        super(props)
+    }
     render(){
+        const states = this.props.value;
         return (
-            <div className="card">Maffia
+            <div className="card">{states}
             </div>
         );
     }
@@ -18,10 +21,14 @@ class Card extends React.Component{
 
 
 
-const play = (state, action) => {
+const play = (state={}, action) => {
     switch(action.type){
         case 'PLAY_CARD':
-            return state + 1;
+            console.log(action);
+            return {
+                text:action.text,
+                id: 56
+            };
         default:
             return state;
     }
@@ -30,19 +37,37 @@ const play = (state, action) => {
 const store = createStore(play);
 
 class Game extends React.Component{
-
+    constructor(props){
+        super(props)
+    }
     render(){
+        const storeState = store.getState();
+        console.log(storeState.text);
         return (
             <div className="content">
                 <h1>Redux With React Through 21 Game</h1>
-                <Card />
-                <Card />
+                <Card value={storeState.text}/>
+                <input ref={node => {
+                 this.input = node;
+             }} />
+                <button onClick={()=>{
+                    store.dispatch({
+                        type: 'PLAY_CARD',
+                        text: this.input.value,
+                        id:1
+                    })
+                    this.input.value = '';
+                }}>Play Card</button>
             </div>
         );
     }
 }
 
 
+const render = () =>{
+    ReactDOM.render(<Game />, document.getElementById('root'));     
+}
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+store.subscribe(render);
+render();
 
